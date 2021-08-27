6daa22ec72256e1458524c958ca64fc5341a6c7f
Dmytro Boichenko <dmitriy.boichenko@karhoo.com>
2021-08-27 18:09:51 +0200

    karhoo local configuration was removed

--- imports_sort_all.sh
+++ imports_sort_all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find $1 -name "*.go" ! -path '*vendor*' | xargs -n1 -P 10 -I {} goimports-reviser -file-path {} -rm-unused -local github.com/karhoo
+find $1 -name "*.go" ! -path '*vendor*' | xargs -n1 -P 10 -I {} goimports-reviser -file-path {} -rm-unused
--- imports_sort_git.sh
+++ imports_sort_git.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git status -s | grep .go | rev | cut -f1 -d ' ' | rev | xargs -n1 -P 10 -I {} goimports-reviser -file-path {} -rm-unused -local github.com/karhoo
+git status -s | grep .go | rev | cut -f1 -d ' ' | rev | xargs -n1 -P 10 -I {} goimports-reviser -file-path {} -rm-unused
