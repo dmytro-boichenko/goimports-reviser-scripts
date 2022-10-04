ce495e6980e32a4dfb5590c64b8ae2616a6a067d
Dmytro Boichenko <dmitriy.boichenko@karhoo.com>
2022-10-04 15:36:04 +0100

    Updates after upgrade version

--- imports_sort_all.sh
+++ imports_sort_all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find . -name "*.go" ! -path '*vendor*' -print0 -exec grep -iL "DO NOT EDIT" {} \; | xargs -0 -n1 -P 10 -I {} goimports-reviser -file-path {} -rm-unused
+find . -name "*.go" ! -path '*vendor*' -print0 -exec grep -iL "DO NOT EDIT" {} \; | xargs -0 -n1 -P 10 -I {} goimports-reviser -rm-unused {}
