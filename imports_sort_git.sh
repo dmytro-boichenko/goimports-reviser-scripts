a322348f6a7c79c750512bdc1f745922380977a7
Dmytro Boichenko <dmitriy.boichenko@karhoo.com>
2023-09-29 10:44:26 +0100

    Updates after upgrade version

--- imports_sort_git.sh
+++ imports_sort_git.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git status -s | grep .go | rev | cut -f1 -d ' ' | rev | xargs -n1 -P 10 -I {} goimports-reviser -rm-unused {}
+git status -s | grep .go | rev | cut -f1 -d ' ' | rev | xargs -n1 -P 10 -I {} goimports-reviser -rm-unused -company-prefixes github.com/karhoo {}
